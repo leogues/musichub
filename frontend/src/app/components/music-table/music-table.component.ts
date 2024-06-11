75238545e311f27211176c31162cba9b1ed9d987
Leonardo Rodrigues <leo_rodrigues1999@hotmail.com>
2024-06-11 15:52:15 -0300

    chore: change baseUrl

--- frontend/src/app/components/music-table/music-table.component.ts
+++ frontend/src/app/components/music-table/music-table.component.ts
@@ -1,10 +1,18 @@
-import { Component, computed, inject, input, signal, WritableSignal } from "@angular/core";
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  signal,
+  WritableSignal,
+} from "@angular/core";
 import { toObservable } from "@angular/core/rxjs-interop";
 
 import { TData, THeader, THeaderWithOrder } from "./music-table";
 import { MusicTableBodyComponent } from "./music-table-body/music-table-body.component";
 import { MusicTableHeaderComponent } from "./music-table-header/music-table-header.component";
 import { MusicTableService } from "./music-table.service";
+import { Subject, takeUntil } from "rxjs";
 
 @Component({
   selector: "app-music-table",
@@ -20,8 +28,10 @@ export class MusicTableComponent {
   isLoading = input<boolean>(false);
   colCount = computed(() => this.header().length);
 
+  private destroy$ = new Subject<void>();
+
   constructor() {
-    this.dataObservable.subscribe((data) => {
+    this.dataObservable.pipe(takeUntil(this.destroy$)).subscribe((data) => {
       this.musicTableService.setData(data);
     });
   }
@@ -31,5 +41,10 @@ export class MusicTableComponent {
 
   handleSort(columnWillOrdered: THeaderWithOrder) {
     this.columnWillOrdered.set({ ...columnWillOrdered });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
